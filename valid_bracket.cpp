19026e600f13e9aa42f142a69dc2724a5a32e625
huangdiankun <huangdiankun@mifpay.com>
2021-05-31 11:44:22 +0800

    leetcode

---
+++ valid_bracket.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        unordered_map<char, char> pairs = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        stack<char> stk;
+        for (char ch: s) {
+            if (pairs.count(ch)) {
+                if (stk.empty() || stk.top() != pairs[ch]) {
+                    return false;
+                }
+                stk.pop();
+            }
+            else {
+                stk.push(ch);
+            }
+        }
+        return stk.empty();
+    }
+};
+
+char pairs(char a) {
+    if (a == '}') return '{';
+    if (a == ']') return '[';
+    if (a == ')') return '(';
+    return 0;
+}
+
+bool isValid(char* s) {
+    int n = strlen(s);
+    if (n % 2 == 1) {
+        return false;
+    }
+    int stk[n + 1], top = 0;
+    for (int i = 0; i < n; i++) {
+        char ch = pairs(s[i]);
+        if (ch) {
+            if (top == 0 || stk[top - 1] != ch) {
+                return false;
+            }
+            top--;
+        } else {
+            stk[top++] = s[i];
+        }
+    }
+    return top == 0;
+}
+
+int main(void)
+{
+	
+	
+	
+}
